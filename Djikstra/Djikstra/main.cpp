4c26c9e036dc90d5ee02d66ee60749fdc14132c6
Bobson1227 <boba.krzysztof98@gmail.com>
2019-03-05 12:28:41 +0100

    First experiments

--- Djikstra/Djikstra/main.cpp
+++ Djikstra/Djikstra/main.cpp
@@ -6,8 +6,50 @@
 
 using namespace std;
 
+struct Edge
+{
+	int 
+	int 
+	string name; 
+	float distance;
+};
+
+struct Vertex
+{
+	string name;
+	vector <Edge> edges;
+	int parent_id = -1;
+	bool visited = 0;
+};
+
 int main()
 {
+	string name1, name2;
+	float dist;
+	int i=0;
+
+	fstream file_data;
+	file_data.open("distances.txt", ios::in);
+
+	if (file_data.good() == false)
+	{
+		cout << "Error with oppening";
+		exit(1);
+	}
+	
+	vector <Vertex> graph;
+
+	while (!file_data.eof())
+	{
+		file_data >> name1 >> name2 >> dist;
+		
+		
+	}
+
+	cout << name1 << name2 << dist << endl;
+
+	file_data.close();
+
 	system("pause");
 	return 0;
 }
