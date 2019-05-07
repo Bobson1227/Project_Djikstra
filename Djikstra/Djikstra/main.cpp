7e81648ab4a15b941aa325e69a34c7b751f3a8e7
Bobson1227 <boba.krzysztof98@gmail.com>
2019-05-07 10:29:57 +0200

    make configuration const

--- Djikstra/Djikstra/main.cpp
+++ Djikstra/Djikstra/main.cpp
@@ -10,42 +10,39 @@
 
 class Arguments
 {
+    Arguments(const char* data_file, const char* inceptive, const char* destination, const char* road_file) 
+        : data_file(data_file), inceptive(inceptive), destination(destination), road_file(road_file)
+    {};
 public:
-    std::string data_file;
-    std::string inceptive;
-    std::string destination;
-    std::string road_file;
+    const std::string data_file;
+    const std::string inceptive;
+    const std::string destination;
+    const std::string road_file;
 
-    void parse_arguments( char *argv[])
-    {
-            data_file = argv[1];
-            inceptive = argv[2];
-            destination = argv[3];
-            road_file = argv[4];
-    }
-};
-
-int main(int argc, char *argv[])
-{
-    Arguments arguments;
-    Graph::Graph graph;
-    
-    try
+    static Arguments parse_arguments( int argc, char *argv[])
     {
         if (argc != 5)
         {
             throw std::invalid_argument("Wrong number of arguments");
         }
-    }
-    catch (const std::invalid_argument& e)
-    {
-        std::cout << "Invalid argument: " << e.what() << std::endl;
-    }
 
-    arguments.parse_arguments(argv);
-    graph.load_graph(arguments.data_file);
-    graph.shortest_way(arguments.inceptive, arguments.destination);
-    graph.write_data(arguments.destination, arguments.road_file);
+        return Arguments(argv[1], argv[2], argv[3], argv[4]);
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    Arguments arguments = Arguments::parse_arguments(argc, argv);
+    Graph::Graph graph;
+    
+    
+        arguments.parse_arguments(argc, argv);
+        graph.load_graph(arguments.data_file);
+        graph.shortest_way(arguments.inceptive, arguments.destination);
+        graph.write_data(arguments.destination, arguments.road_file);
+    
+    
+
     
 
     system("pause");
