c1adee413c829a8e6081308bbb2e4a345725124e
Bobson1227 <boba.krzysztof98@gmail.com>
2019-05-12 11:06:55 +0200

    change variables name

--- Djikstra/Djikstra/main.cpp
+++ Djikstra/Djikstra/main.cpp
@@ -10,13 +10,13 @@
 
 class Arguments
 {
-    Arguments(const char* data_file, const char* inceptive, const char* destination, const char* road_file) 
-        : data_file(data_file), inceptive(inceptive), destination(destination), road_file(road_file)
+    Arguments(const char* data_file, const char* source, const char* target, const char* road_file) 
+        : data_file(data_file), source(source), target(target), road_file(road_file)
     {};
 public:
     const std::string data_file;
-    const std::string inceptive;
-    const std::string destination;
+    const std::string source;
+    const std::string target;
     const std::string road_file;
 
     static Arguments parse_arguments( int argc, char *argv[])
@@ -34,17 +34,12 @@ int main(int argc, char *argv[])
 {
     Arguments arguments = Arguments::parse_arguments(argc, argv);
     Graph::Graph graph;
+        
+    arguments.parse_arguments(argc, argv);
+    graph.load_graph(arguments.data_file);
+    graph.shortest_way(arguments.source, arguments.target);
+    graph.write_data(arguments.target, arguments.road_file);
     
-    
-        arguments.parse_arguments(argc, argv);
-        graph.load_graph(arguments.data_file);
-        graph.shortest_way(arguments.inceptive, arguments.destination);
-        graph.write_data(arguments.destination, arguments.road_file);
-    
-    
-
-    
-
     system("pause");
     return 0;
 }
